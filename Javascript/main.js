4d3b461badc4e0b4722710dab18160638ff091f9
zpremium <zdravkoms99@gmail.com>
2021-05-20 12:40:28 +0200

    acabado

--- Javascript/main.js
+++ Javascript/main.js
@@ -3,10 +3,11 @@ async function getWeather() {
       const response = await fetch('https://www.el-tiempo.net/api/json/v2/provincias/08/municipios/08019');
       if (response) {
           const jsonResponse = await response.json();
+          const lugar = jsonResponse.municipio.NOMBRE;
           const today = jsonResponse.fecha;
           const weather = jsonResponse.stateSky.description.toLowerCase();
           const temperature = jsonResponse.temperatura_actual;
-          const tiempo = `Hoy, ${today},  habrá  ${weather} de ${temperature}º`;
+          const tiempo = `En ${lugar} a día < ${today} >,  habrá  ${weather} y estamos a: ${temperature}º`;
           document.getElementById('tiempo').innerHTML = tiempo;
       }
   } catch (error) {
